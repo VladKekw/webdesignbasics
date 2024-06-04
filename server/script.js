91479402725a19c512b0b46674ed1683a268c564
Vladyslav Sorokolit <wwlddss@gmail.com>
2024-06-04 18:37:11 +0300

    removed iframe video from index.html

--- server/script.js
+++ server/script.js
@@ -1,15 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var form = document.getElementById("consForm");
-
-  form.addEventListener("submit", function(event) {
-      event.preventDefault();
-
-      var email = document.getElementById("email").value;
-      var name = document.getElementById("name").value;
-
-      alert("Дякуємо " + name + "\n" + "Необхідна інформація надіслана на " + email);
-  });
-});
 
 document.addEventListener("DOMContentLoaded", function() {
   const blogForm = document.getElementById("blogForm");
